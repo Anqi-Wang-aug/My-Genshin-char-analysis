b9977286fd141a537b8693fa817d38ae7a4887d2
Anqi Wang <angelwang11a@outlook.com>
2022-06-28 14:57:56 -0400

    Delete 06-28-patch.sql

--- 06-28-patch.sql
+++
@@ -1,7 +0,0 @@
--- Yanfei Elemental Burst Name modified
-UPDATE SKILL SET NAME = 'Done Deal' WHERE SKILL_ID = 18;
--- Diona Elemental Burst Name modified
-UPDATE SKILL SET NAME = 'Signature Mix' WHERE SKILL_ID = 30;
-
-
-SELECT * FROM SKILL;
